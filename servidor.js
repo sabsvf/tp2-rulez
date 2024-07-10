1fdad6d00aa8095008f18632777838df80d746c0
sabsvf <f.sabrina@gsuite.iff.edu.br>
2024-07-10 10:39:58 -0300

    exercicio 10/07

--- servidor.js
+++ servidor.js
@@ -3,7 +3,18 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 app.get('/', (request, response) => {
-    let numero = 5
-    response.send(`A letra x é: $(numero)`)
+    let resultado = ""
+    response.render(`conta`, {resultado})
 })
-app.listen(8080)
+app.post('/salvar', (req, response) => {
+    let number = parseFloat(req.body.massa)
+    let number2 = parseFloat(req.body.MassaMolecular)
+    let resultado = (number / number2)
+    response.render(`conta`, {resultado})
+
+})
+
+const PORT = 8080
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
